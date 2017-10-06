715d3f7aeff1497b9b577cb588b2c458e62da211
Begum.Yazici <yazicibegum@gmail.com>
2017-10-06 19:55:59 +0300

    This program makes a time account.

---
+++ basicexamp/src/basicexamples/times.java
@@ -0,0 +1,34 @@
+package basicexamples;
+
+import java.util.Scanner;
+
+/**
+ * Created by asus1 on 6.10.2017.
+ */
+public class times {
+    public static void main(String[] args) {
+
+        Scanner entry = new Scanner(System.in);
+        System.out.print("Saati giriniz: ");
+        int hour1=entry.nextInt();
+        System.out.print("Dakikayi giriniz: ");
+        int minute1=entry.nextInt();
+        System.out.print("Fark(dk) giriniz: ");
+        int differenceMinute=entry.nextInt();
+
+        int minute2=differenceMinute%60;
+        int hour2=differenceMinute/60;
+
+        int sumHour=hour1+hour2;
+        if((sumHour>24)){
+            sumHour = sumHour - 24;
+        }
+
+        int sumMinute=minute1+minute2;
+        if(sumMinute>60){
+            sumMinute=sumMinute-60;
+        }
+
+        System.out.println("Sonuc: "+sumHour+"."+sumMinute);
+    }
+}
